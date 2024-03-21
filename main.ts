80b0c5020c36f46b2f893fa84ba768bd4c665a6d
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-21 21:11:39 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,41 @@
+// if- else statement 
+let weather = "Hot";
+if (weather === "Cold"){
+    console.log("Drink a lot of water.");
+}
+else if (weather === "hot"){
+    console.log("Wear light clothes");
+}
+else {
+    console.log("stay at home")
+}
+
+let studing = "maths";
+if (studing === "science"){
+    console.log("be open minded");
+}
+else if (studing == "social studies"){
+    console.log("remember every timeline");
+}
+else {
+    console.log("the only way to learn mathetics is to do mathematics");
+}
+
+let moodSwings = "true"
+if (moodSwings){
+    console.log("mood swings are continuously shifting, like the weather.");
+}
+else {
+    console.log("mood swings in progress");
+}
+
+let workingFrom = "online"
+if (workingFrom == "Online"){
+    console.log("no social interaction");
+}
+else if (workingFrom == "office"){
+    console.log("maintain proper professional career");
+}
+else {
+    console.log("more comfortable");
+}
